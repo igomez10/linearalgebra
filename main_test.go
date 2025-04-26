2d0e46766b8bee1402f81d6f6c89cd79abeb283c
Ignacio Gomez <ignacio.gomez@deliveryhero.com>
2025-04-26 13:01:56 +0200

    add benchmark

--- main_test.go
+++ main_test.go
@@ -2937,3 +2937,23 @@ func TestSaveMatrix(t *testing.T) {
 		})
 	}
 }
+
+var table = []struct {
+	input int
+}{
+	{input: 100},
+	{input: 1_000},
+	{input: 10_000},
+	{input: 100_000},
+	{input: 1_000_000},
+}
+
+func BenchmarkSave(b *testing.B) {
+	for _, v := range table {
+		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				GenerateIdentityMatrix(v.input)
+			}
+		})
+	}
+}
